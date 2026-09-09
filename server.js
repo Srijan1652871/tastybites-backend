2b26da944519b770114db515f4018ad6c44fb952
Srijan1652871 <srijanjaimaakali@gmail.com>
2026-09-10 01:33:14 +0530

    cors fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const userRoutes = require("./src/routes/user.routes");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://tastybites-frontend-nu.vercel.app","http://localhost:5173"],
     credentials: true,
   }),
 );
